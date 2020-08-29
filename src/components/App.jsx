797e933b933720b21b2f53eb3735cd08e16f59d2
backendAlgo <mirshod.mirjonov@mail.ru>
2020-08-29 20:48:12 +0500

    main functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+
 function App() {
+    const [notes, setNotes] = useState([]);
+    function addNote(value) {
+        setNotes((prev) => {
+            return [
+                ...prev,value
+            ]
+        })
+    }
+    function deleteNote(index) {
+        setNotes((prev) => {
+            return prev.filter((element, i) => {
+                return i !== index;
+            })
+        })
+    }
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea  addNote={addNote}/>
+        {notes.map(({title, content}, index) => {
+            return (
+                <Note key={index} id={index} title={title} content={content} deleteNote={deleteNote}/>
+            )
+        })}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
-  return (
+function CreateArea({addNote}) {
+    const [value, setValue] = useState({title: "", content: ""});
+    function handleChange(e) {
+        const newValue = e.target.value;
+        const name = e.target.name;
+        setValue((prev) => {
+            return {
+                ...prev,
+                [name]: newValue
+            }
+        })
+    }
+    return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+      <form onSubmit={(e) => {
+          e.preventDefault();
+          addNote(value);
+          setValue({title: "", content: ""});
+      }}>
+        <input onChange={handleChange} name="title" placeholder="Title" value={value.title}/>
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={value.content}/>
         <button>Add</button>
       </form>
     </div>
-  );
+    );
 }
 
 export default CreateArea;
