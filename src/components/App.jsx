9cd96c7f7ea569f520c6a0f2a5b82f2d41b59804
backendAlgo <mirshod.mirjonov@mail.ru>
2020-08-29 21:51:19 +0500

    new desing features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
-
 function App() {
-    const [notes, setNotes] = useState([]);
-    function addNote(value) {
-        setNotes((prev) => {
-            return [
-                ...prev,value
-            ]
-        })
-    }
-    function deleteNote(index) {
-        setNotes((prev) => {
-            return prev.filter((element, i) => {
-                return i !== index;
-            })
-        })
-    }
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea  addNote={addNote}/>
-        {notes.map(({title, content}, index) => {
-            return (
-                <Note key={index} id={index} title={title} content={content} deleteNote={deleteNote}/>
-            )
-        })}
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
